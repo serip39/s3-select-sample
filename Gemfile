f75ec41956e1c3533843976470c966b95fd06932
serip39 <seri@fusic.co.jp>
2023-06-01 17:39:34 +0900

    feat: install aws-sdk-s3 && add upload_sample.rb

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem "aws-sdk-s3"
+# 以下のエラーが発生したので、oxを追加
+# Unable to find a compatible xml library. Ensure that you have installed or added to your Gemfile one of ox, oga, libxml, nokogiri or rexml (RuntimeError)
+gem "ox"
---
+++ upload_sample.rb
@@ -0,0 +1,11 @@
+require 'aws-sdk-s3'
+
+profile_name = 'seri-dev'
+bucket_name = 's3-slect-test'
+
+s3_client = Aws::S3::Client.new(profile: profile_name, region: 'ap-northeast-1')
+
+key = 'target_sample.csv'
+
+# サンプル用のデータをアップロードする
+s3_client.put_object(bucket: bucket_name, key: key, body: File.read('sample.csv'))
